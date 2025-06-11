1c13c94d241eb24c06b72afd8b531a0cfa376951
Zuza Gronska <zgronska@gmail.com>
2025-06-11 21:13:35 +0200

    chore: update Stylelint config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,3 +1,23 @@
 export default {
-    extends: ['stylelint-config-recommended', 'stylelint-config-html/astro']
+    extends: [
+        'stylelint-config-recommended',
+        'stylelint-config-html/astro',
+        'stylelint-config-property-sort-order-smacss'
+    ],
+    plugins: [
+        'stylelint-plugin-logical-css',
+        'stylelint-rem-over-px',
+        'stylelint-declaration-block-no-ignored-properties'
+    ],
+    rules: {
+        'plugin/use-logical-properties-and-values': [
+            true,
+            {
+                severity: 'warning',
+                ignore: ['overflow-y', 'overflow-x']
+            }
+        ],
+        'plugin/declaration-block-no-ignored-properties': true,
+        'rem-over-px/rem-over-px': true
+    }
 }
